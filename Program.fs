9f07bb775dfdfc40d35254facfb1d5d558c21d96
Wiktor Nowicki <59111798+wiktornowicki@users.noreply.github.com>
2020-03-15 17:14:53 +0100

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -9,5 +9,5 @@ let main _ =
     //root function body
     //NOTE: the "_" arguement is the more universal replacement for "varg". It is completely up to you which one you will choose. I recommend using the underscore.
     Console.ReadKey(true) |> ignore; //the "|> ignore" operation should be used on functions, which have an unnecessary return.
-    //If your function is meant to return a value (an arithmetical operation, for example), this is the only case when an explicit value to be returned is required.
+    //If your function is meant to return a value (an arithmetic operation, for example), this is the only case when an explicit value to be returned is required.
     0; //The final expression in a function is always the return code.
